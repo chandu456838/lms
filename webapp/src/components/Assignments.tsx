7df872331fd0e002d6c0f3216f1027c4330a021a
chandu456838 <153174899+chandu456838@users.noreply.github.com>
2023-12-30 13:46:52 +0530

    Update Assignments.tsx
    readonly

--- webapp/src/components/Assignments.tsx
+++ webapp/src/components/Assignments.tsx
@@ -2,7 +2,7 @@ type AssignmentsProps = {
   assignmentFiles?: string[];
 };
 
-function Assignments(props: AssignmentsProps) {
+function Assignments(props: Readonly<Props>) {
   if (!props.assignmentFiles?.length) {
     return (
       <div className="prose">
